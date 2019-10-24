67e6d1282a2b55cd2ac224c1d7fb5ff8865b54e9
Francesco Frassinelli <francesco.frassinelli@nina.no>
2019-10-24 17:10:55 +0200

    Add support for directories

--- extract_from_url/__init__.py
+++ extract_from_url/__init__.py
@@ -1,7 +1,8 @@
 import urllib.request
 import libarchive  # libarchive-c
+from pathlib import Path
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
 def extract_from_url(url, progress_bar=None):
@@ -11,6 +12,9 @@ def extract_from_url(url, progress_bar=None):
         progress_bar.total = int(request.headers.get("Content-length", 0))
     with libarchive.stream_reader(request.fp) as stream:
         for member in stream:
+            if member.isdir:
+                Path(member.path).mkdir()
+                continue
             with open(member.path, "wb") as fp:
                 for block in member.get_blocks():
                     fp.write(block)
