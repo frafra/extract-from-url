cf5679bbe4f6baefafbe12837cd9ff6a0ccb813a
Francesco Frassinelli <francesco.frassinelli@nina.no>
2019-10-25 10:29:23 +0200

    Accept open_function as argument

--- extract_from_url/__init__.py
+++ extract_from_url/__init__.py
@@ -4,8 +4,10 @@ from pathlib import Path
 
 __version__ = "0.2.0"
 
+_mkdir = lambda path: Path(path).mkdir()
+_open = open
 
-def extract_from_url(url, progress_bar=None):
+def extract_from_url(url, progress_bar=None, mkdir_f=_mkdir, open_f=_open):
     request = urllib.request.urlopen(url)
     if progress_bar is not None:
         progress_bar.desc = filename = request.info().get_filename()
@@ -13,10 +15,13 @@ def extract_from_url(url, progress_bar=None):
     with libarchive.stream_reader(request.fp) as stream:
         for member in stream:
             if member.isdir:
-                Path(member.path).mkdir()
-                continue
-            with open(member.path, "wb") as fp:
-                for block in member.get_blocks():
-                    fp.write(block)
-                    if progress_bar is not None:
-                        progress_bar.update(len(block))
+                mkdir_f(member.path)
+            elif member.isfile:
+                fp = open_f(member.path, "wb")
+                try:
+                    for block in member.get_blocks():
+                        fp.write(block)
+                        if progress_bar is not None:
+                            progress_bar.update(len(block))
+                finally:
+                    fp.close()
